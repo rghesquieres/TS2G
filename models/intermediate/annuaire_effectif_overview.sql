3981ada4e3887e8473a5b05030b3529ec8afa89a
Camille MANEL <camille.manel@gmail.com>
2025-06-13 13:34:48 +0000

    statut annuaire annuaire effectif overview et IDF

---
+++ models/intermediate/annuaire_effectif_overview.sql
@@ -0,0 +1,18 @@
+SELECT
+    uai,
+    type_etablissement,
+    an.code_dep,
+    code_region,
+    ulis,
+    segpa,
+    rep,
+    has_pial,
+    options_speciales,
+    nb_eleves,
+    geo.departement
+FROM {{ ref('annuaire_effectif') }} as an
+LEFT JOIN {{ ref('departement_geographie') }} as geo
+    USING(code_dep)
+WHERE nb_eleves IS NOT NULL
+    AND geo.departement IS NOT NULL
+
