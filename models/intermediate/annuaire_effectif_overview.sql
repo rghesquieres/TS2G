e2508b1e638568db5052b79af4b171979fda6a73
Camille MANEL <camille.manel@gmail.com>
2025-06-13 15:16:22 +0000

    ajouts des geographies sur IVAL IVAC + ANNUAIRE IDF ET OVERVIEW

--- models/intermediate/annuaire_effectif_overview.sql
+++ models/intermediate/annuaire_effectif_overview.sql
@@ -1,3 +1,4 @@
+WITH dep AS (
 SELECT
     uai,
     type_etablissement,
@@ -10,10 +11,18 @@ SELECT
     options_speciales,
     nb_eleves,
     statut,
-    geo.departement
+    geo.departement,
+    geo.dep_geography
 FROM {{ ref('annuaire_effectif') }} as an
 LEFT JOIN {{ ref('departement_geographie') }} as geo
     USING(code_dep)
 WHERE nb_eleves IS NOT NULL
     AND geo.departement IS NOT NULL
-
+)
+SELECT
+    dep.*,
+    reg.region,
+    reg.reg_geography,
+FROM dep
+LEFT JOIN {{ ref('stg_clean__region_geographie') }} as reg
+    USING(code_region)
--- models/intermediate/ival_ips_IDF.sql
+++ models/intermediate/ival_ips_IDF.sql
@@ -1,5 +1,7 @@
-SELECT ips.*,
-iva.pk,
+WITH ivalips AS(
+SELECT
+
+ips.*,
 iva.nb_0,
 iva.nb_presents_all,
 iva.taux_reussite_all,
@@ -15,6 +17,18 @@ iva.va_taux_acces_2_0,
 iva.va_taux_mention_all,
 iva.va_taux_acces_1_bac,
 iva.va_taux_acces_0_bac,
+
 FROM {{ ref('ips_lycees_all') }} AS ips
 LEFT JOIN {{ ref('iva_lycees_all') }} AS iva
 ON CONCAT (ips.annee,"_", ips.uai) = iva.pk
+
+WHERE ips.code_dep IN ("075", "077", "078", "091", "092", "093", "094", "095")
+)
+
+SELECT
+    ivalips.*,
+    dep.departement,
+    dep.dep_geography
+FROM ivalips
+LEFT JOIN {{ ref('departement_geographie') }} as dep
+    USING(code_dep)
